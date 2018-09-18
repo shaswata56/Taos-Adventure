5a1b72c45ff647a1511195820add867f85cc8fee
shaswata56 <shaswata56@gmail.com>
2018-09-18 14:31:37 +0600

    Creating tao.h.
    this tao header include constructor, destructor and some function defination for draw and move Tao. Also some protected class variable declearation.

---
+++ sources/tao.h
@@ -0,0 +1,25 @@
+#ifndef TAO_H_INCLUDED
+#define TAO_H_INCLUDED
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+
+class Tao
+{
+public:
+    Tao(pos , int, int);
+    ~cTao();
+    void fDrawTao(Game*, pos);
+    void fMoveTao(Game*, pos, int);
+
+
+protected:
+    sf::Texture* T1;
+    int x, y, r;
+    sf::CircleShape* tShape;
+
+private:
+
+
+};
+
+#endif // TAO_H_INCLUDED
