16f0bfbcbe2b1ec9a5f9863d20406282b9fad372
shaswata56 <shaswata56@gmail.com>
2018-09-20 04:26:04 +0600

    creating enemy.h

---
+++ sources/enemy.h
@@ -0,0 +1,13 @@
+#ifndef ENEMY_H_INCLUDED
+#define ENEMY_H_INCLUDED
+
+class Enemy
+{
+    public:
+        Enemy();
+        virtual ~Enemy();
+    protected:
+    private:
+};
+
+#endif // ENEMY_H_INCLUDED
