08566900d62db1d8bf939f07907f56b9705d91ea
shaswata56 <shaswata56@gmail.com>
2018-09-20 04:12:14 +0600

    component.h banailam

---
+++ sources/component.h
@@ -0,0 +1,13 @@
+#ifndef COMPONENT_H_INCLUDED
+#define COMPONENT_H_INCLUDED
+
+class Component
+{
+    public:
+        Component();
+        virtual ~Component();
+    protected:
+    private:
+};
+
+#endif // COMPONENT_H_INCLUDED
